13340667dd1674baaa654bcaaf414398a9d0ae13
Tyler Rice <tylerrice@Tylers-MBP-2.cable.rcn.com>
2021-09-19 18:59:21 -0500

    second commit - adding materialize

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ let $correctanswer;
 let $incorrectanswerA;
 let $incorrectanswerB;
 let $incorrectanswerC;
-
+let gameData;
+let randomArray;
 
 // grab html elements
 const $category = $('.category')
@@ -19,71 +20,102 @@ const $t = $('#true')
 const $f = $('#false')
 const $answer = $('.answer')
 
-let gameData;
+const play = function () {
+    getData()
+}
+
+const getData = function (event) {
+    $.ajax(`https://opentdb.com/api.php?amount=11&difficulty=easy`).then(function (data) {
+
+        gameData = data;
+
+        console.log(gameData)
+        randomizer()
+        result()
+
+
+        if (gameData.results[0].type == "multiple") {
+            $('.t-f').remove()
+            renderMult()
+        } else {
+            renderTf()
+        }
+    })
+}
+
+const collectQs = function(){
 
 
 
-$.ajax(`https://opentdb.com/api.php?amount=11&difficulty=easy`).then(function (data) {
 
-    gameData = data;
-    console.log(gameData)
+}
 
-    $category.html(gameData.results[0].category)
-    $question.html(gameData.results[0].question)
 
+const randomizer = function () {
     $correctanswer = gameData.results[0].correct_answer
     $incorrectanswerA = gameData.results[0].incorrect_answers[0]
     $incorrectanswerB = gameData.results[0].incorrect_answers[1]
     $incorrectanswerC = gameData.results[0].incorrect_answers[2]
-    if (gameData.results[0].type == "multiple") {
-        $('.t-f').remove()
 
+    let answerArray = [$correctanswer, $incorrectanswerA, $incorrectanswerB, $incorrectanswerC]
 
-        let answerArray = [$correctanswer, $incorrectanswerA, $incorrectanswerB, $incorrectanswerC]
-
-
-        // invoke fisher yates algorithm to randomize array    
-        let shuffleArray = answerArray => {
-            for (let i = answerArray.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                const temp = answerArray[i];
-                answerArray[i] = answerArray[j]
-                answerArray[j] = temp;
-            }
-            return answerArray
+    // invoke fisher yates algorithm to randomize array    
+    let shuffleArray = answerArray => {
+        for (let i = answerArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = answerArray[i];
+            answerArray[i] = answerArray[j]
+            answerArray[j] = temp;
         }
-        // store the random array in a new variable    
-        let randomArray = shuffleArray(answerArray)
-        console.log(randomArray)
+        return answerArray
+    }
+    // store the random array in a new variable    
+    randomArray = shuffleArray(answerArray)
+    console.log(randomArray)
 
-        $a.html(randomArray[0])
-        $b.html(randomArray[1])
-        $c.html(randomArray[2])
-        $d.html(randomArray[3])
-    } else {
-        $('.multiple-choice').remove()
-        if (gameData.results[0].correct_answer == "True") {
-            $t.html(gameData.results[0].correct_answer)
-        } else if (gameData.results[0].correct_answer !== "True") {
-            $t.html(gameData.results[0].incorrect_answers)
+}
+
+const renderMult = function () {
+
+    $category.html(gameData.results[0].category)
+    $question.html(gameData.results[0].question)
+
+    $a.html(randomArray[0])
+    $b.html(randomArray[1])
+    $c.html(randomArray[2])
+    $d.html(randomArray[3])
+}
+
+const renderTf = function () {
+
+    $category.html(gameData.results[0].category)
+    $question.html(gameData.results[0].question)
+
+
+    if (gameData.results[0].correct_answer == "True") {
+        $t.html(gameData.results[0].correct_answer)
+    } else if (gameData.results[0].correct_answer !== "True") {
+        $t.html(gameData.results[0].incorrect_answers)
+    }
+}
+
+const result = function () {
+    $('.answer').on('click', function () {
+        console.log($(this).text())
+        console.log($correctanswer)
+        if ($(this).text() == $correctanswer) {
+            console.log('congrats')
+            alert('congrats')
+        } else {
+            console.log('wrong')
+            alert('wrong')
         }
+    })
+}
 
-    }
-})
+play()
 
-
-$('.answer').on('click', function(){
-    console.log($(this).text())
-    console.log($correctanswer)
-    if ($(this).text() == $correctanswer) {
-        console.log('congrats')
-    } else {
-        console.log('wrong')
-    }
-})
-
-
-
-
-
-
+$('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
