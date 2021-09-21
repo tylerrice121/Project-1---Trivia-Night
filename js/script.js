7fedfd2b05cfde751e2f1295c8717c56c493b559
Tyler Rice <tylerrice@Tylers-MBP-2.cable.rcn.com>
2021-09-21 14:28:14 -0500

    buttons added pages formatted

--- js/script.js
+++ js/script.js
@@ -13,27 +13,25 @@ const play = function () {
 }
 
 const getData = function () {
-    $.ajax(`https://opentdb.com/api.php?amount=10&difficulty=easy`).then(function (data) {
+    $.ajax(`https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple`).then(function (data) {
 
         gameData = data;
 
-        //create questions array
         arrayQs = gameData.results
-        console.log(arrayQs)
 
         render()
-        result()
+        getResult()
 
     })
 }
 
 function render() {
-    
+
 
     const html = arrayQs.map(function (q) {
-        console.log(q.correct_answer)
+
         correctAnswerArray.push(q.correct_answer)
-        console.log(correctAnswerArray)
+
 
         if (q.type == "multiple") {
 
@@ -55,68 +53,46 @@ function render() {
             }
             // store the random array in a new variable    
             randomArray = shuffleArray(answerArray)
-            console.log(randomArray)
 
-
-            return `<section class ="question1">
-                <h2>Category</h2>
-                <p class="category">${q.category}</p>
-                <h2>Question</h2>
-                <p class="question">${q.question}</p>
-                <table class="table table-hover" style="cursor:pointer">
-                    <tbody class="multiple-choice">
-                        <tr id="row1">
-                            <th scope="row">A</th>
-                            <th class="answer" id="a">${randomArray[0]}</th>
-                        </tr>
-                        <tr id="row2">
-                            <th scope="row">B</th>
-                            <th class="answer" id="b">${randomArray[1]}</th>
-                        </tr>
-                        <tr id="row3">
-                            <th scope="row">C</th>
-                            <th class="answer" id="c">${randomArray[2]}</th>
-                        </tr>
-                        <tr id="row4">
-                            <th scope="row">D</th>
-                            <th class="answer" id="d">${randomArray[3]}</th>
-                        </tr>
-                    </tbody>
-                </table>      
-            </section>`
-
-        } else if (q.type === "boolean") {
-            return `<section class ="question1">
-                <h2>Category</h2>
-                <p class="category">${q.category}</p>
-                <h2>Question</h2>
-                <p class="question">${q.question}</p>
-                <table class="table table-hover" style="cursor:pointer">
-                    <tbody class="t-f">
-                        <tr id="row5">
-                            <th class="answer" id="true">True</th>
-                        </tr>
-                        <tr id="row6">
-                            <th class="answer" id="false">False</th>
-                        </tr>
-                    </tbody>
-                </table>      
-                </section>`
-            if (q.correct_answer == "True") {
-                $('#true').html(q.correct_answer)
-            } else if (q.correct_answer !== "True") {
-                $('#true').html(q.incorrect_answers)
-            }
-        }       
+            return `
+            <div class = "section">
+                <div class ="question1">
+                    <h2>Category</h2>
+                    <p class="category">${q.category}</p>
+                    <h2>Question</h2>
+                    <p class="question">${q.question}</p>
+                    <table class="table table-hover" style="cursor:pointer">
+                        <tbody class="multiple-choice">
+                            <tr id="row1">
+                                <th scope="row">A</th>
+                                <th class="answer" id="a">${randomArray[0]}</th>
+                            </tr>
+                            <tr id="row2">
+                                <th scope="row">B</th>
+                                <th class="answer" id="b">${randomArray[1]}</th>
+                            </tr>
+                            <tr id="row3">
+                                <th scope="row">C</th>
+                                <th class="answer" id="c">${randomArray[2]}</th>
+                            </tr>
+                            <tr id="row4">
+                                <th scope="row">D</th>
+                                <th class="answer" id="d">${randomArray[3]}</th>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <a class="waves-effect waves-light btn">Next Question</a>      
+                </div>
+            </div>    `
+                
+        }
+        
     })
-    $('body').html(html);
-    console.log(html)
+    $('main').html(html);
 }
-// if ($(this).text() == $correctanswer)
-function result() {
+
+function getResult() {
     $('.answer').on('click', function () {
-        console.log($(this).text())
-        console.log(correctAnswerArray)
         if (correctAnswerArray.includes($(this).text())) {
             console.log('congrats')
             alert('congrats')
@@ -127,7 +103,18 @@ function result() {
     })
 }
 
-console.log(correctAnswerArray)
+function scroll(){
+    $('a').on('click', 'button', function(){
+        let next = $(this).closest(".question1").next(),
+        section = $(this).closest('html')
+
+		section.animate({
+            scrollTop: $('html').scrollTop() + fuller.offset().top
+        
+        }, 700);
+})
+}
+scroll()
 
 play()
 
