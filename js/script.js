2b2f261dc6b1ca2b96d77d17daa15f90fa7d3071
Tyler Rice <tylerrice@Tylers-MBP-2.cable.rcn.com>
2021-09-22 09:18:58 -0500

    each page is distinguished with own class

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ let gameData;
 let correctAnswerArray = [];
 let score = 0;
 
+
 const play = function () {
     getData()
 }
@@ -29,7 +30,7 @@ const getData = function () {
 function render() {
 
 
-    const html = arrayQs.map(function (q) {
+    const html = arrayQs.map(function (q, index) {
 
             correctAnswerArray.push(q.correct_answer)
 
@@ -58,7 +59,7 @@ function render() {
                 if (randomArray[0] === $correctanswer) {
                     console.log(`the correct answer is A`)
                     return `
-                    <div class = "section">
+                    <div class = "${index} section">
                         <div class ="question1">
                             <h2>Category</h2>
                             <p class="category">${q.category}</p>
@@ -67,20 +68,20 @@ function render() {
                             <table class="table table-hover" style="cursor:pointer">
                                 <tbody class="multiple-choice">
                                     <tr id="row1">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="a">A) ${randomArray[0]}</a>
-                                        <a class="waves-effect waves-light btn-large correct-answer answer-button-on" style="display: block" d="a">A) ${randomArray[0]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="a">A) ${randomArray[0]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large correct-answer answer-button-on" style="display: block" d="a">A) ${randomArray[0]}</a>
                                     </tr>
                                     <tr id="row2">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="b">B) ${randomArray[1]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="b">B) ${randomArray[1]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="b">B) ${randomArray[1]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="b">B) ${randomArray[1]}</a>
                                     </tr>
                                     <tr id="row3">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="c">C) ${randomArray[2]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="c">C) ${randomArray[2]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="c">C) ${randomArray[2]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="c">C) ${randomArray[2]}</a>
                                     </tr>
                                     <tr id="row4">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="d">D) ${randomArray[3]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="d">D) ${randomArray[3]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="d">D) ${randomArray[3]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="d">D) ${randomArray[3]}</a>
                                     </tr>
                                 </tbody>
                             </table>
@@ -90,7 +91,7 @@ function render() {
                 } else if (randomArray[1] === $correctanswer) {
                     console.log(`the correct answer is B`)
                     return `
-                    <div class = "section">
+                    <div class = "${index} section">
                         <div class ="question1">
                             <h2>Category</h2>
                             <p class="category">${q.category}</p>
@@ -99,20 +100,20 @@ function render() {
                             <table class="table table-hover" style="cursor:pointer">
                                 <tbody class="multiple-choice">
                                     <tr id="row1">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="a">A) ${randomArray[0]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="a">A) ${randomArray[0]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="a">A) ${randomArray[0]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="a">A) ${randomArray[0]}</a>
                                     </tr>
                                     <tr id="row2">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="b">B) ${randomArray[1]}</a>
-                                        <a class="waves-effect waves-light btn-large correct-answer answer-button-on" style="display: block" id="b">B) ${randomArray[1]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="b">B) ${randomArray[1]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large correct-answer answer-button-on" style="display: block" id="b">B) ${randomArray[1]}</a>
                                     </tr>
                                     <tr id="row3">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="c">C) ${randomArray[2]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="c">C) ${randomArray[2]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="c">C) ${randomArray[2]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="c">C) ${randomArray[2]}</a>
                                     </tr>
                                     <tr id="row4">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="d">D) ${randomArray[3]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="d">D) ${randomArray[3]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="d">D) ${randomArray[3]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="d">D) ${randomArray[3]}</a>
                                     </tr>
                                 </tbody>
                             </table>
@@ -122,7 +123,7 @@ function render() {
                 } else if (randomArray[2] === $correctanswer) {
                     console.log(`the correct answer is C`)
                     return `
-                    <div class = "section">
+                    <div class = "${index} section">
                         <div class ="question1">
                             <h2>Category</h2>
                             <p class="category">${q.category}</p>
@@ -131,20 +132,20 @@ function render() {
                             <table class="table table-hover" style="cursor:pointer">
                                 <tbody class="multiple-choice">
                                     <tr id="row1">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="a">A) ${randomArray[0]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="a">A) ${randomArray[0]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="a">A) ${randomArray[0]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="a">A) ${randomArray[0]}</a>
                                     </tr>
                                     <tr id="row2">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="b">B) ${randomArray[1]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="b">B) ${randomArray[1]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="b">B) ${randomArray[1]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="b">B) ${randomArray[1]}</a>
                                     </tr>
                                     <tr id="row3">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="c">C) ${randomArray[2]}</a>
-                                        <a class="waves-effect waves-light btn-large correct-answer answer-button-on" style="display: block" id="c">C) ${randomArray[2]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="c">C) ${randomArray[2]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large correct-answer answer-button-on" style="display: block" id="c">C) ${randomArray[2]}</a>
                                     </tr>
                                     <tr id="row4">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="d">D) ${randomArray[3]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="d">D) ${randomArray[3]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="d">D) ${randomArray[3]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="d">D) ${randomArray[3]}</a>
                                     </tr>
                                 </tbody>
                             </table>
@@ -154,7 +155,7 @@ function render() {
                 } else if (randomArray[3] === $correctanswer) {
                     console.log(`the correct answer is D`)
                     return `
-                    <div class = "section">
+                    <div class = "${index} section">
                         <div class ="question1">
                             <h2>Category</h2>
                             <p class="category">${q.category}</p>
@@ -163,20 +164,20 @@ function render() {
                             <table class="table table-hover" style="cursor:pointer">
                                 <tbody class="multiple-choice">
                                     <tr id="row1">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="a">A) ${randomArray[0]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="a">A) ${randomArray[0]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="a">A) ${randomArray[0]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="a">A) ${randomArray[0]}</a>
                                     </tr>
                                     <tr id="row2">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="b">B) ${randomArray[1]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="b">B) ${randomArray[1]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="b">B) ${randomArray[1]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="b">B) ${randomArray[1]}</a>
                                     </tr>
                                     <tr id="row3">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="c">C) ${randomArray[2]}</a>
-                                        <a class="waves-effect waves-light btn-large answer-button-on" style="display: block" id="c">C) ${randomArray[2]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="c">C) ${randomArray[2]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large answer-button-on" style="display: block" id="c">C) ${randomArray[2]}</a>
                                     </tr>
                                     <tr id="row4">
-                                        <a class="waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="d">D) ${randomArray[3]}</a>
-                                        <a class="waves-effect waves-light btn-large correct-answer answer-button-on" style="display: block" id="d">D) ${randomArray[3]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large disabled answer-button-off" style="display: none" id="d">D) ${randomArray[3]}</a>
+                                        <a class="${index} waves-effect waves-light btn-large correct-answer answer-button-on" style="display: block" id="d">D) ${randomArray[3]}</a>
                                     </tr>
                                 </tbody>
                             </table>
@@ -187,31 +188,124 @@ function render() {
         
     })
 $('main').html(html);
-//add section numbers to questions
-function addPgNumbers(){
-    $(html[0]).html(`.waves-effect`).prepend(`<p>Page1</p>`)
-}
 
-console.log($(html[0]).html(`.waves-effect`))
-addPgNumbers()
-}
+console.log(html)
 
+}
 
 function getResult() {
-    $(document).on('click', '.waves-effect', function () {
-        // $(this).each($('#answer-button-off')).show();
-        console.log($(this).closest($('div')).children())
-        $('.answer-button-off').each(function(){
-            $(this).closest($('div')).children().show();
+    //PG1
+        $('.0').on('click', '.waves-effect', function () {
+        $('.0.answer-button-off').each(function(){
+            $(this).show();
         })
-        $('.answer-button-on').each(function(){
+        $('.0.answer-button-on').each(function(){
             $(this).hide();
         })
+    })
+//
+    //PG2
+        $('.1').on('click', '.waves-effect', function () {
 
-        console.log(this)
+        $('.1.answer-button-off').each(function(){
+            $(this).show();
+        })
+        $('.1.answer-button-on').each(function(){
+            $(this).hide();
+        })
+    })
+//
+    //PG3
+        $('.2').on('click', '.waves-effect', function () {
 
+        $('.2.answer-button-off').each(function(){
+            $(this).show();
+        })
+        $('.2.answer-button-on').each(function(){
+            $(this).hide();
+        })
+    })
+//
+    //PG4
+        $('.3').on('click', '.waves-effect', function () {
+
+        $('.3.answer-button-off').each(function(){
+            $(this).show();
+        })
+        $('.3.answer-button-on').each(function(){
+            $(this).hide();
+        })
+    })
+//
+    //PG5
+        $('.4').on('click', '.waves-effect', function () {
+
+        $('.4.answer-button-off').each(function(){
+            $(this).show();
+        })
+        $('.4.answer-button-on').each(function(){
+            $(this).hide();
+        })
+    })
+//
+    //PG6
+        $('.5').on('click', '.waves-effect', function () {
+
+        $('.5.answer-button-off').each(function(){
+            $(this).show();
+        })
+        $('.5.answer-button-on').each(function(){
+            $(this).hide();
+        })
+    })
+//
+    //PG7
+        $('.6').on('click', '.waves-effect', function () {
+
+        $('.6.answer-button-off').each(function(){
+            $(this).show();
+        })
+        $('.6.answer-button-on').each(function(){
+            $(this).hide();
+        })
+    })
+//
+    //PG8
+        $('.7').on('click', '.waves-effect', function () {
+
+        $('.7.answer-button-off').each(function(){
+            $(this).show();
+        })
+        $('.7.answer-button-on').each(function(){
+            $(this).hide();
+        })
+    })
+//
+    //PG9
+        $('.8').on('click', '.waves-effect', function () {
+
+        $('.8.answer-button-off').each(function(){
+            $(this).show();
+        })
+        $('.8.answer-button-on').each(function(){
+            $(this).hide();
+        })
+    })
+//
+    //PG10
+        $('.9').on('click', '.waves-effect', function () {
+
+        $('.9.answer-button-off').each(function(){
+            $(this).show();
+        })
+        $('.9.answer-button-on').each(function(){
+            $(this).hide();
+        })
+    })
+//
+    console.log(this)
+    $(document).on('click', '.waves-effect', function () {
         if ($(this).hasClass('correct-answer')) {
-            $(this).closest($('tr')).css("background-color", "green")
             score = score + 1
             console.log('congrats')
             console.log($(this).closest($('tr')))
@@ -222,11 +316,13 @@ function getResult() {
             alert('wrong')
         }
         console.log(score)
-    })
-}
+    })    
+
+    }
 
 
-console.log(randomArray)
+
+// console.log(randomArray)
 // function scroll(){
 //     $('a').on('click', 'button', function(){
 //         let next = $(this).closest(".question1").next(),
